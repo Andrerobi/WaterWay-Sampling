225462c25432610a3589f186355e9c3e26748328
Andrerobi <56739084+Andrerobi@users.noreply.github.com>
2023-12-10 15:47:26 -0500

    Add files via upload

--- Script.js
+++ Script.js
@@ -4,14 +4,10 @@ var svg = d3.select("svg"),
     originalWidth = 500,
     originalHeight = 500;
 
-    var scaleFactor = 1.5;
-    // Adjusted dimensions
-    var width = originalWidth * scaleFactor,
-        height = originalHeight * scaleFactor;
-
-// Set new SVG dimensions
-svg.attr("width", width)
-    .attr("height", height);
+// Color scale
+var color = d3.scaleOrdinal()
+    .domain(["Decha", "Kannika", "Chai", "Busarakhan", "Kohsoom", "Boonsri", "Achara", "Somchair", "Tansanee", "Sakda"])
+    .range(["#402D54", "#D18975", "#8FD175", "#FFEE8D", "#D175D1", "#D1A275", "#D1D175", "#D1758F", "#75D1D1", "#D1758F"]);
 
 
 // Markers = CIRCLES
@@ -26,8 +22,190 @@ var markers = [
   { x: 250, y: 350, group: "Somchair", size: 10 }, //Somchair 
   { x: 250, y: 510, group: "Tansanee", size: 10 }, // Sakda
   { x: 400, y: 640, group: "Sakda", size: 10 }, //Sakda 
-  // Add more circle positions as needed
 ];
+
+
+var scaleFactor = 1.5;
+// Adjusted dimensions
+var width = originalWidth * scaleFactor,
+    height = originalHeight * scaleFactor;
+
+// Set new SVG dimensions
+svg.attr("width", width)
+    .attr("height", height);
+
+// Assuming your data has a 'size' property that determines the circle size
+var sizeDomain = d3.extent(markers, function(d) { return d.size; });
+
+// Add a scale for bubble size
+var sizeScale = d3.scaleLinear()
+    .domain(sizeDomain)  // Adjust based on your data
+    .range([4, 50]);  // Size in pixels, adjust the range based on your desired size range
+
+
+console.log(parsedData);
+
+//testing
+var chaiData = parsedData.filter(function(d) {
+  return d.location === "Chai";
+});
+console.log("Data for locations named 'Chai':", chaiData);
+
+var uniqueYears = [...new Set(parsedData.map(d => d.year_of_sample_date))];
+var uniqueMonths = [...new Set(parsedData.map(d => d.month_of_sample_date))];
+
+var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// Create radio buttons for years
+createRadioButtons(uniqueYears);
+
+// Create a slider for months
+createMonthSlider(uniqueMonths);
+
+// Function to create radio buttons
+function createRadioButtons(years) {
+  var radioContainer = d3.select("#radio-container");
+
+  radioContainer.selectAll("input")
+    .data(years)
+    .enter()
+    .append("label")
+    .text(function(d) { return d; })
+    .append("input")
+    .attr("type", "radio")
+    .attr("name", "year")
+    .attr("value", function(d) { return d; })
+    .property("checked", function(d, i) { return i === 0; }) // Default to the first year
+    .on("change", function() {
+      // Handle year change event
+      var selectedYear = this.value;
+      var dataForSelectedYear = parsedData.filter(function(d) {
+        return d.year_of_sample_date === +selectedYear;
+      });
+
+      // Log all data points for the selected year to the console
+      console.log("Data for year " + selectedYear + ":", dataForSelectedYear);
+
+      updateVisualization(dataForSelectedYear);
+    });
+}
+
+
+// Function to create a slider
+function createMonthSlider(months) {
+  var sliderContainer = d3.select("#slider-container");
+
+  sliderContainer.select("#month-slider")
+      .on("input", function() {
+          // Handle month slider input event
+          var selectedMonthNumber = +this.value;
+          var selectedMonthName = monthNames[selectedMonthNumber - 1];
+
+          // Update the displayed month 
+          sliderContainer.select("#selected-month").text(selectedMonthName);
+
+          // Continue with the updateVisualization function or any other actions
+          updateVisualization(parsedData);
+      });
+}
+
+// Function to create a slider
+// function createSlider() {
+//   var sliderContainer = d3.select("#slider-container");
+//   var slider = sliderContainer.append("input")
+//     .attr("type", "range")
+//     .attr("min", 1)
+//     .attr("max", 12)
+//     .attr("value", 1)
+//     .on("input", function() {
+//         // Handle slider input event
+//         updateVisualization(data);
+//     });
+
+// // Add labels for the slider
+// sliderContainer.append("label")
+//     .text("Month:");
+//   }
+
+ // Function to update visualization based on selected year and month
+function updateVisualization(selectedYear, selectedMonth) {
+  console.log("Selected Year:", selectedYear);
+  console.log("Selected Month (Number):", selectedMonth);
+
+  // Convert numeric month to string representation
+  var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var selectedMonthName = monthNames[selectedMonth - 1];
+  //console.log("Selected Month (String):", selectedMonthName);
+
+   // Filter data based on selected year and month
+   var filteredData = parsedData.filter(function (d) {
+    return d.year_of_sample_date === selectedYear && d.month_of_sample_date === selectedMonthName &&
+        d.x !== null && d.y !== null && d.value !== null;  // Add additional conditions as needed
+});
+
+
+
+   console.log("Filtered Data:", filteredData);
+  //console.log("Filtered Data By Location:", filteredDataByLocation);
+
+
+ // Update circle sizes based on the 'measure value' for the selected data subset
+ sizeScale.domain([0, d3.max(filteredData, function (d) { return d.value; })]);
+
+ 
+
+//HAVING TROUBLE WITH THIS PART ~~~~~~
+// Add or update circles
+var circles = svg.selectAll("circle")
+    .data(filteredData, function(d, i) { 
+      return i !== null && i !== undefined ? i : d.group;
+    });
+    
+
+    circles.enter()
+    .append("circle")
+    .attr("cx", function (d) { return d.x; })
+    .attr("cy", function (d) { return d.y; })
+    .merge(circles)
+    .transition()
+    .duration(1000)
+    .attr("r", function(d) {
+      var sizeValue = sizeScale(d.value);
+      return isNaN(sizeValue) ? 0 : sizeValue;
+  })  // Use sizeScale for the bubble size
+    .style("fill", function (d) { return color(d.group); });
+
+//circles.exit().remove();
+
+
+}
+
+
+// Assuming you have a function to initialize the visualization
+function initializeVisualization() {
+  // Your initialization code here
+}
+
+createMonthSlider([...new Set(parsedData.map(d => d.month_of_sample_date))]);
+
+// Event listener for radio buttons
+d3.selectAll("input[name='year']").on("change", function () {
+  var selectedYear = +this.value; // Convert to number
+  var selectedMonth = +d3.select("#month-slider").property("value"); // Get selected month from the slider
+
+  updateVisualization(selectedYear, selectedMonth);
+});
+
+// Event listener for the initial setup
+d3.select("#month-slider").on("input", function () {
+  var selectedYear = +d3.select("input[name='year']:checked").node().value; // Get selected year from radio buttons
+  var selectedMonth = +this.value; // Get selected month from the slider
+
+  updateVisualization(selectedYear, selectedMonth);
+});
+
+// Initialize the visualization
+initializeVisualization();
 
 // Load external data and boot
 d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson", function(data){
@@ -35,41 +213,17 @@ d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/w
     // Filter data
     data.features = data.features.filter( function(d){return d.properties.name=="WaterWay"} )
 
-    // Create a color scale
-    var color = d3.scaleOrdinal()
-      .domain(["Decha", "Kannika", "Chai", "Busarakhan", "Kohsoom", "Boonsri", "Achara", "Somchair", "Tansanee", "Sakda" ])
-      .range([ "#402D54", "#D18975", "#8FD175", "#FFEE8D", "#D175D1", "#D1A275", "#D1D175", "#D1758F", "#75D1D1", "#D1758F"])
+    
+
+    // // Create a color scale
+    // var color = d3.scaleOrdinal()
+    //   .domain(["Decha", "Kannika", "Chai", "Busarakhan", "Kohsoom", "Boonsri", "Achara", "Somchair", "Tansanee", "Sakda" ])
+    //   .range([ "#402D54", "#D18975", "#8FD175", "#FFEE8D", "#D175D1", "#D1A275", "#D1D175", "#D1758F", "#75D1D1", "#D1758F"])
 
     // Add a scale for bubble size
     var size = d3.scaleLinear()
       .domain([1,100])  // What's in the data
       .range([ 4, 50])  // Size in pixel
-
-
-      //Can probably delete this
-    ///////////////////////////////////////
-    // List of words with associated group
-    // var words = [
-    // { word: "Word1", group: "A" },
-    // { word: "Word2", group: "A" },
-    // { word: "Word3", group: "A" },
-    // { word: "Word4", group: "A" },
-    // { word: "Word5", group: "A" }
-    // ];
-    // // Create a div for the word list
-    // var wordListDiv = d3.select("#wordList");
-
-    // // Populate the word list
-    // var wordItems = wordListDiv.selectAll(".wordItem")
-    // .data(words)
-    // .enter()
-    // .append("div")
-    // .classed("wordItem", true)
-    // .html(function(d) {
-    // return '<input type="radio" name="' + d.group + '" value="' + d.word + '">' + d.word;
-    // })
-    // .style("font-size", "14px");
-    ///////////////////////////////////////////////
     
   
     // Add the background image from current directory
@@ -81,18 +235,28 @@ d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/w
 
 
     // Add circles:
-svg.selectAll("myCircles")
-.data(markers)
-.enter()
-.append("circle")
-.attr("class", function (d) { return d.group; })
-.attr("cx", function (d) { return d.x; })  // Use x coordinate on the image
-.attr("cy", function (d) { return d.y; })  // Use y coordinate on the image
-.attr("r", function (d) { return size(d.size); })
-.style("fill", function (d) { return color(d.group); })
-.attr("stroke", function (d) { return color(d.group); })
-.attr("stroke-width", 3)
-.attr("fill-opacity", .4);
+    svg.selectAll("myCircles")
+    .data(markers)
+    .enter()
+    .append("circle")
+    .attr("class", function (d) { return d.group; })
+    .attr("cx", function (d) {
+        console.log("X Coordinate:", d.x);
+        return d.x;
+    })
+    .attr("cy", function (d) {
+        console.log("Y Coordinate:", d.y);
+        return d.y;
+    })
+    .attr("r", function (d) {
+        console.log("Circle Size:", sizeScale(d.size));
+        return sizeScale(d.size);
+    })
+    .style("fill", function (d) { return color(d.group); })
+    .attr("stroke", function (d) { return color(d.group); })
+    .attr("stroke-width", 3)
+    .attr("fill-opacity", .4);
+  
 
 
     // This function is gonna change the opacity and size of selected and unselected circles
